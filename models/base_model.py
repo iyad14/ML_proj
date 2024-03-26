a048ef37baa9c5cceade5b453d9b17ea593764fb
Ralph Aouad <ralph.aouad@outlook.com>
2024-03-26 22:49:32 +0200

    Add docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,94 @@
 import joblib
+from typing import Any, Dict, List, Optional, Union
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 class BaseModel:
-    def __init__(self, X_train, X_test, y_train, y_test):
+    def __init__(self, X_train: Any, X_test: Any, y_train: Any, y_test: Any) -> None:
+        """
+        Initialize the base model with training and testing data.
+
+        :param X_train: Training data features
+        :param X_test: Testing data features
+        :param y_train: Training data labels
+        :param y_test: Testing data labels
+        """
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
         self.model = None
-        self.results = []
+        self.results: List[Dict[str, Union[int, float]]] = []
 
-    def fit(self):
+    def fit(self) -> None:
+        """
+        Fit the model on the training data. Subclasses should implement this method.
+        """
         raise NotImplementedError("Subclasses should implement this method!")
 
-    def predict(self, X):
-        if self.model:
-            return self.model.predict(X)
-        raise Exception("Model not trained!")
+    def predict(self, X: Any) -> Any:
+        """
+        Predict the labels for the given data.
 
-    def evaluate(self):
+        :param X: Data to predict labels for
+        :return: Predicted labels
+        """
+        if not self.model:
+            raise Exception("Model not trained!")
+        return self.model.predict(X)
+
+    def evaluate(self) -> Dict[str, Union[float, Any]]:
+        """
+        Evaluate the model on the testing data.
+
+        :return: A dictionary containing evaluation metrics
+        """
         y_pred = self.predict(self.X_test)
         metrics = {
             'Accuracy': round(accuracy_score(self.y_test, y_pred), 3),
             'Precision': round(precision_score(self.y_test, y_pred, average='weighted'), 3),
             'Recall': round(recall_score(self.y_test, y_pred, average='weighted'), 3),
             'F1 Score': round(f1_score(self.y_test, y_pred, average='weighted'), 3),
-            'Confusion Matrix': confusion_matrix(self.y_test, y_pred)
+            # 'Confusion Matrix': confusion_matrix(self.y_test, y_pred)
         }
         return metrics
 
-    def save_model(self, file_path):
-        if self.model:
-            joblib.dump(self.model, file_path)
+    def save_model(self, file_path: str) -> None:
+        """
+        Save the trained model to a file.
 
-    def load_model(self, file_path):
+        :param file_path: The path to save the model file
+        """
+        if not self.model:
+            raise Exception("No model to save!")
+        joblib.dump(self.model, file_path)
+
+    def load_model(self, file_path: str) -> None:
+        """
+        Load a model from a file.
+
+        :param file_path: The path to the model file
+        """
         self.model = joblib.load(file_path)
 
-    def get_best_configuration(self):
-        if self.results:
-            return max(self.results, key=lambda x: x['Accuracy'])
-        return None
+    def get_best_configuration(self) -> Optional[Dict[str, Union[int, float]]]:
+        """
+        Get the best configuration based on the stored results.
 
-    def run(self, **kwargs):
+        :return: A dictionary containing the best configuration parameters and their corresponding accuracy
+        """
+        return max(self.results, key=lambda x: x['Accuracy']) if self.results else None
+
+    def run(self, **kwargs) -> Optional[Dict[str, Union[int, float]]]:
+        """
+        Run the model training and return the best configuration. Subclasses should implement this method.
+
+        :return: The best configuration parameters and their corresponding accuracy
+        """
         self.fit()
         return self.get_best_configuration()
 
-    def test_configurations(self):
+    def test_configurations(self) -> None:
+        """
+        Test different configurations of the model. Subclasses should implement this method.
+        """
         raise NotImplementedError("Subclasses should implement this method!")
--- models/knn_model.py
+++ models/knn_model.py
@@ -1,31 +1,51 @@
+import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from math import sqrt
 from .base_model import BaseModel
 
 class KNNModel(BaseModel):
-    def __init__(self, X_train, X_test, y_train, y_test):
+    def __init__(self, X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray) -> None:
+        """
+        Initialize the KNN model with training and testing data.
+
+        :param X_train: Training data features
+        :param X_test: Testing data features
+        :param y_train: Training data labels
+        :param y_test: Testing data labels
+        """
         super().__init__(X_train, X_test, y_train, y_test)
 
-    def fit(self, n_neighbors=5):
+    def fit(self, n_neighbors: int = 5) -> None:
+        """
+        Fit the KNN model using the specified number of neighbors.
+
+        :param n_neighbors: Number of neighbors to use
+        """
         self.model = KNeighborsClassifier(n_neighbors=n_neighbors)
         self.model.fit(self.X_train, self.y_train)
 
-    def test_configurations(self):
+    def test_configurations(self) -> None:
+        """
+        Test different configurations of the KNN model to find the best number of neighbors.
+        """
         n = len(self.X_train)
-        sqrt_n = int(sqrt(n))
-        k_range = range(1, sqrt_n + 1)  # Update k_range to be from 1 to sqrt(n)
-        
-        for k in k_range:
-            self.fit(n_neighbors=k)
-            y_pred = self.predict(self.X_test)
-            accuracy = accuracy_score(self.y_test, y_pred)
-            self.results.append({'K': k, 'Accuracy': accuracy})
-        
-        best_config = self.get_best_configuration()
-        if best_config:
-            self.fit(n_neighbors=best_config['K'])  # Refit model with best K value
+        sqrt_n = int(np.sqrt(n))
+        parameters = {'n_neighbors': range(1, sqrt_n + 1)}
 
-    def run(self, **kwargs):
+        grid_search = GridSearchCV(KNeighborsClassifier(), parameters, cv=5, scoring='accuracy', n_jobs=-1)
+        grid_search.fit(self.X_train, self.y_train)
+
+        best_parameters = grid_search.best_params_
+        self.results.append({'K': best_parameters['n_neighbors'], 'Accuracy': grid_search.best_score_})
+
+        # Refit the model with the best number of neighbors
+        self.fit(n_neighbors=best_parameters['n_neighbors'])
+
+    def run(self, **kwargs) -> dict:
+        """
+        Run the KNN model training and hyperparameter tuning process.
+
+        :return: The best configuration parameters and their corresponding accuracy
+        """
         self.test_configurations()
         return self.get_best_configuration()
