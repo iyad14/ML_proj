9f955df9d09417e30e48f17f433cd2418c66b090
Ralph Aouad <ralph.aouad@outlook.com>
2024-03-26 22:27:44 +0200

    Update KNN Model

--- models/knn_model.py
+++ models/knn_model.py
@@ -1,26 +1,31 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from math import sqrt
+from .base_model import BaseModel
 
-class KNNModel:
+class KNNModel(BaseModel):
     def __init__(self, X_train, X_test, y_train, y_test):
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = y_test
-        self.results = []
+        super().__init__(X_train, X_test, y_train, y_test)
 
-    def test_configurations(self, k_range):
+    def fit(self, n_neighbors=5):
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbors)
+        self.model.fit(self.X_train, self.y_train)
+
+    def test_configurations(self):
+        n = len(self.X_train)
+        sqrt_n = int(sqrt(n))
+        k_range = range(1, sqrt_n + 1)  # Update k_range to be from 1 to sqrt(n)
+        
         for k in k_range:
-            model = KNeighborsClassifier(n_neighbors=k)
-            model.fit(self.X_train, self.y_train)  # Fit model on training data
-            y_pred = model.predict(self.X_test)  # Predict on test data
-            accuracy = accuracy_score(self.y_test, y_pred)  # Evaluate accuracy on test data
+            self.fit(n_neighbors=k)
+            y_pred = self.predict(self.X_test)
+            accuracy = accuracy_score(self.y_test, y_pred)
             self.results.append({'K': k, 'Accuracy': accuracy})
-    
-    def get_best_configuration(self):
-        best_result = max(self.results, key=lambda x: x['Accuracy'])
-        return best_result
-    
-    def run(self, k_range=range(1, 31)):
-        self.test_configurations(k_range)
+        
+        best_config = self.get_best_configuration()
+        if best_config:
+            self.fit(n_neighbors=best_config['K'])  # Refit model with best K value
+
+    def run(self, **kwargs):
+        self.test_configurations()
         return self.get_best_configuration()
